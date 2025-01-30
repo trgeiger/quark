dd8e8dcfb2faba60f6ca631bbd59bda1f8df7105
Tayler Geiger <mail@tayler.dev>
2025-01-30 12:06:52 -0600

    Set up personal repo override
    Put all nvidia driver install logic in a separate script
    rather than keep in the Containerfile.

--- install-nvidia.sh
+++ install-nvidia.sh
@@ -6,29 +6,33 @@ RELEASE="$(rpm -E '%fedora.%_arch')"
 MAJOR_VERSION="$(rpm -E '%fedora')"
 IMAGE_NAME="${1:-quark-open}"
 NVIDIA_VERSION="${2:-stable}"
+USE_PERSONAL_REPO="${3:-false}"
+ENABLED_REPO=""
 
 cd /tmp
 if [[ "${NVIDIA_VERSION}" == "beta" ]]; then
-    echo "Installing beta driver"
-    dnf5 -y config-manager addrepo --from-repofile=https://developer.download.nvidia.com/compute/cuda/repos/fedora${QUARK_VERSION}/x86_64/cuda-fedora${QUARK_VERSION}.repo
-    if [[ "${IMAGE_NAME}" == "quark-open" ]]; then
-        dnf5 -y install --allowerasing nvidia-open \
-    ; else
-        dnf5 -y install cuda-drivers \
-    ; fi
-#dnf5 -y install rpmfusion-nonfree-release-rawhide && \
-# ENABLED_REPO=rpmfusion-nonfree-rawhide \
-else \
-    echo "Installing stable driver" && \
-    ls -alR /tmp/akmods-rpms && \
-    #dnf5 -y install --enable-repo="$ENABLED_REPO" \
-    dnf5 -y install \
-        xorg-x11-drv-nvidia-cuda \
-        xorg-x11-drv-nvidia-cuda-libs \
-        nvidia-vaapi-driver \
-        libva-utils \
-        vdpauinfo \
-        libva-nvidia-driver \
-        mesa-vulkan-drivers.i686 \
-        /tmp/akmods-rpms/kmods/kmod-nvidia-*.rpm \
-; fi 
+    echo "Installing beta driver\n"
+    if [[ "${USE_PERSONAL_REPO}" == "true" ]]; then
+        echo "Installing from personal repo\n"
+        curl -Lo /etc/yum.repos.d/tayler-nvidia.repo https://raw.githubusercontent.com/trgeiger/nvidia-kmod-cache/refs/heads/main/tayler-nvidia.repo
+    else
+        dnf5 -y install rpmfusion-nonfree-release-rawhide
+        ENABLED_REPO="rpmfusion-nonfree-rawhide"
+    fi
+else
+    echo "Installing stable driver\n"
+fi
+
+ls -alR /tmp/akmods-rpms
+dnf5 -y install --enable-repo="$ENABLED_REPO" \
+    xorg-x11-drv-nvidia \
+    xorg-x11-drv-nvidia-cuda \
+    xorg-x11-drv-nvidia-cuda-libs \
+    xorg-x11-drv-nvidia-power \
+    nvidia-vaapi-driver \
+    libva-utils \
+    vdpauinfo \
+    libva-nvidia-driver \
+    mesa-vulkan-drivers.i686 \
+    /tmp/akmods-rpms/kmods/kmod-nvidia-*.rpm
+
