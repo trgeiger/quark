39d98a40266a5498cb72d17f96f8417355d48537
Tayler Geiger <mail@tayler.dev>
2025-08-06 11:48:48 -0500

    Use personal override repo for nvidia 580

--- install-nvidia.sh
+++ install-nvidia.sh
@@ -12,14 +12,12 @@ ENABLED_REPO=""
 cd /tmp
 if [[ "${NVIDIA_VERSION}" == "beta" ]]; then
     echo "Installing beta driver\n"
+    dnf5 -y copr enable kwizart/nvidia-driver-rawhide
+    sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/fedora-updates-testing.repo
     if [[ "${USE_PERSONAL_REPO}" == "true" ]]; then
         echo "Installing from personal repo\n"
         curl -Lo /etc/yum.repos.d/tayler-nvidia.repo https://raw.githubusercontent.com/trgeiger/nvidia-kmod-cache/refs/heads/main/tayler-nvidia.repo
     else
-        echo "Installing from kwizart repo\n"
-        dnf5 -y copr enable kwizart/nvidia-driver-rawhide
-        sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/fedora-updates-testing.repo
-        # Install fake-grubby for beta until f42
         dnf5 -y install rpmfusion-nonfree-release-rawhide
         ENABLED_REPO="rpmfusion-nonfree-rawhide"
     fi
